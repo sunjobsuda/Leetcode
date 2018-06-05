80a760cd9c16d5f1fe65fc46c0b8a36664b94a9f
sunjob <sunjob_suda@163.com>
2018-06-05 10:27:07 +0800

    11. Container With Most Water

--- Container With Most Water.c
+++ Container With Most Water.c
@@ -1,15 +1,4 @@
 int maxArea(int* height, int heightSize) {
-    // int i, j, size;
-    // int maxSize = 0;
-    // for(i = 0;i < heightSize; i++){
-    //     if((heightSize-1-i) * height[i] < maxSize)continue;
-    //     for(j = i + 1; j < heightSize; j++){
-    //         size = height[i] > height[j] ? height[j]*(j-i) : height[i]*(j-i);
-    //         maxSize = maxSize < size ? size : maxSize;
-    //     }
-    // }
-    // return maxSize;
-    
     int i, j, cap;
     int max = 0;
     for(i = 0, j = heightSize - 1; i < j;){
